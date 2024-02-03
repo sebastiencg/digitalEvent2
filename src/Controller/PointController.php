53216125665aef8572ad1cad778d5fa3a6e92c7c
sebastien <miantamag@gmail.com>
2024-02-03 17:06:00 +0100

    new

--- src/Controller/PointController.php
+++ src/Controller/PointController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+use App\Entity\ParticipantOfDraw;
 use App\Entity\Question;
+use App\Repository\ParticipantOfDrawRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,14 +27,18 @@ class PointController extends AbstractController
 
         $entityManager->flush();
 
-        return $this->json([],Response::HTTP_OK);
+        return $this->json("ok",Response::HTTP_OK);
     }
 
-    #[Route('/score', name: 'app_score')]
-    public function score(): Response
+    #[Route('/score/', name: 'app_score')]
+    public function score(ParticipantOfDrawRepository $participantOfDrawRepository): Response
     {
+        $lastParticipantOfDraw = $participantOfDrawRepository->findOneBy([], ['id' => 'DESC']);
+        $score=[];
+        foreach ($lastParticipantOfDraw->getParticipant() as $participant){
+            $score[] = $participant->getPoint();
+        }
+        return $this->json($score, Response::HTTP_OK, [], ['groups' => 'game:read-one']);
 
-        return $this->render('party/score.html.twig', [
-        ]);
     }
 }
